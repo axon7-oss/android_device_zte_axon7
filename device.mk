00cabb6f6ad46672ebd6fd4145d1c60a409b97e0
Aaron Kling <webgeek1234@gmail.com>
2019-05-01 22:27:54 -0500

    axon7: Build Trust HAL
    Change-Id: I3175dde7567c243b6601f5cb0140b955872fcbdd

--- device.mk
+++ device.mk
@@ -405,6 +405,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-8996-normal.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8996-normal.conf \
     $(LOCAL_PATH)/configs/thermal-engine-8996-perf.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8996-perf.conf
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # VNDK
 PRODUCT_PACKAGES += \
     libicuuc.vendor \
