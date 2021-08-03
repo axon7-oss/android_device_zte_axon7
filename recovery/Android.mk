6907233168d8d0ee574de251a81e9a780f1cd98b
A2L5E0X1 <alexed25@protonmail.com>
2021-08-03 22:38:33 +0200

    axon7: recovery_updater: remove LOCAL_MODULE_TAGS := eng
    Change-Id: I7270cacd376ba0783727b6f449eee6dbb8a17501

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -5,5 +5,4 @@ LOCAL_C_INCLUDES := $(call project-path-for,recovery) \
     $(call project-path-for,recovery)/updater/include
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_axon7
-LOCAL_MODULE_TAGS := eng
 include $(BUILD_STATIC_LIBRARY)
