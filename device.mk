6f7a91714f6ada7294f9072dc87f7e9f4f86de12
A2L5E0X1 <alexed25@protonmail.com>
2021-08-03 22:03:58 +0200

    axon7: define a namespace for device tree
    Change-Id: If09e8cd08a6304f84e48421069dae4504d75c9a3

--- device.mk
+++ device.mk
@@ -378,6 +378,9 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
+
 # Treble
 PRODUCT_USE_VNDK_OVERRIDE := true
 
