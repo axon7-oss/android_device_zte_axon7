0f41e42a064cb8c4902fe9abcf5d2a782691fe27
A2L5E0X1 <alexed25@protonmail.com>
2021-08-04 00:45:09 +0200

    axon7: recovery_updater: add missing include dir
    Change-Id: I08cb1f088fb6ed3a4d38833694d8f729f263a60a

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -3,7 +3,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_C_INCLUDES := \
     bootable/recovery \
-    bootable/recovery/updater/include
+    bootable/recovery/updater/include \
+    bootable/recovery/edify/include
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_axon7
 include $(BUILD_STATIC_LIBRARY)
