e43923600c71a1bcf60e33331ebe8c7232f6f216
A2L5E0X1 <alexed25@protonmail.com>
2021-08-04 13:04:05 +0200

    axon7: update kernel flags
    Change-Id: Icec025000c99816ef9f6f7be24222ab7e908db18

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,16 +62,17 @@ DEVICE_MANIFEST_FILE := $(PLATFORM_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(PLATFORM_PATH)/compatibility_matrix.xml
 
 # Kernel
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_PAGESIZE := 4096
-BOARD_KERNEL_TAGS_OFFSET := 0x00000100
-BOARD_RAMDISK_OFFSET     := 0x01000000
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=512M@0-0xffffffff
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --second_offset 0x00f00000 --tags_offset 0x00000100
 TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_SOURCE := kernel/zte/msm8996
-TARGET_KERNEL_CONFIG := lineageos_axon7_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
+
+# Kernel Source
+TARGET_KERNEL_CONFIG := lineageos_axon7_defconfig
+TARGET_KERNEL_SOURCE := kernel/zte/msm8996
 
 # Qualcomm support
 BOARD_USES_QCOM_HARDWARE := true
