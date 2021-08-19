87a43b4e67a3fb338af28c9afb45d79b92b90e4f
A2L5E0X1 <alexed25@protonmail.com>
2021-08-19 20:01:45 +0200

    axon7: update audio hals to 5.0
    Change-Id: I4a2ec26da81dc19f80c32275b2500420dd61f4ad

--- device.mk
+++ device.mk
@@ -99,8 +99,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
+    android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.0-impl \
     audio.a2dp.default \
     audio_amplifier.msm8996 \
